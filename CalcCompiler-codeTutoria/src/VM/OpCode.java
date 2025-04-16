bd8959f68ee20f03856fe753dcec68ef52c8ca45
dluz1991 <a65873@ualg.pt>
2025-04-16 10:11:47 +0100

    new instrutions in opcode
    introduçao de 5 opcodes novos para variaveis e alteraçao da ordem dos opcodes

--- CalcCompiler-codeTutoria/src/VM/OpCode.java
+++ CalcCompiler-codeTutoria/src/VM/OpCode.java
@@ -6,56 +6,61 @@ package VM;
 
 public enum OpCode {
     // single-byte instructions, just the OpCode: no arguments
-    //Integer instructions
+    //Intructions with 1 argument (5 bytes)
     iconst   (1),
+    dconst   (1),
+    sconst   (1),
+    //Integer instructions
+    iprint   (0),
     iuminus  (0),
     iadd     (0),
     isub     (0),
     imult    (0),
     idiv     (0),
     imod (0),
-    ilt   (0),
-    ileq (0),
     ieq   (0),
     ineq (0),
-    itos   (0),
+    ilt   (0),
+    ileq (0),
     itod  (0),
-    iprint   (0),
-
-    //String instructions
-    sconst   (1),
-    sconcat   (0),
-    sprint   (0),
-    seq   (0),
-    sneq(0),
-
+    itos   (0),
     //Real instructions
-    dconst   (1),
+    dprint   (0),
     duminus  (0),
     dadd     (0),
     dsub     (0),
     dmult    (0),
     ddiv     (0),
-    dmod (0),
-    dlt(0),
-    dleq(0),
     deq   (0),
     dneq (0),
+    dlt(0),
+    dleq(0),
     dtos   (0),
-    dprint   (0),
-
+    //String instructions
+    sprint   (0),
+    sconcat   (0),
+    seq   (0),
+    sneq(0),
     //Boolean instructions
     tconst   (0),
     fconst   (0),
-    not     (0),
-    and     (0),
-    or      (0),
+    bprint   (0),
     beq   (0),
     bneq (0),
+    and     (0),
+    or      (0),
+    not     (0),
     btos (0),
-    bprint   (0),
-
-    halt     (0) // stop execution
+    // Instructions for variables, 1 or 2 arguments (5 bytes)
+    jump(2),
+    jumpf(2),
+    galloc(1),
+    gload(1),
+    gstore(1),
+    // stop execution
+    halt     (0),
+    //nao existe no op code da virtual machine
+    dmod (0),
     ;
 
 
